9f89988d63432554330538df6a7f3470cfab2f14
Tui <tuipopenoe@gmail.com>
2015-06-25 20:50:13 -0500

    Queue data structure

---
+++ Linked_Lists/queue.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Tui Popenoe
+# queue.py 
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        """O(1)"""
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        """O(1)"""
+        return self.items.pop()
+
+    def size(self):
+        """O(1)"""
+        return len(self.items)
